80abff5bc5007712ac80d939c79938cad3d4f30a
Abheek Gulati <akg29@njit.edu>
2018-05-22 16:05:14 +0530

    completed function, pending testing/verification

--- BackPropagate.py
+++ BackPropagate.py
@@ -1,42 +1,67 @@
 def back_propagate(target_output, network_output, number_of_output_neurons, number_of_hidden_neurons, 
 					number_of_input_neurons, hidden_to_output_layer_wts, input_to_hidden_layer_wts, network_input,
-					hidden_layer_output, learning_rate):
+					hidden_layer_input, hidden_layer_output, learning_rate):
 
-	new_hidden_to_output_layer_wts = []
-	delta_output_layer = []
 
-	temp_wts = []
+	#computing delta terms first:
+	delta_output_layer = []				#delta = target_output - actual_neuron_output
 	for i in range(number_of_output_neurons):
-		temp = []
-		delta = network_output[i] - target_output[i]
+		delta = 0
+		delta = target_output[i] - network_output[i]
 		delta_output_layer.append(delta)
-		neuron_output = network_output[i]		#to compute a(1-a)
-		output_factor = neuron_output * (1 - neuron_output)
-		weight_delta = learning_rate * delta  * output_factor	#alpha * delta * a(1-a)
-		for j in range(number_of_hidden_neurons):
-			temp.append(weight_delta + hidden_to_output_layer_wts[j][i])	#multiplying above product by x, i.e., o/p of previous layer
-		temp_wts.append(temp)
 
-	for j in range(len(temp_wts[0])):
-		temp = []
-		for i in range(len(temp_wts)):
-			temp.append(temp_wts[i][j])
-		new_hidden_to_output_layer_wts.append(temp)
-
-	
-	new_input_to_hidden_layer_wts = []
-
-	#compute delta for the hidden layer using delta of the output layer
-	
-	delta_hidden_layer = []
+	delta_hidden_layer = []				#delta += weight_from_hidden_to_output_layer * output_neurons_delta
 	for i in range(number_of_hidden_neurons):
 		delta = 0
 		for j in range(number_of_output_neurons):
-			delta += new_hidden_to_output_layer_wts[i][j] * delta_output_layer[j]
+			delta += hidden_to_output_layer_wts[i][j] * delta_output_layer[j]
 		delta_hidden_layer.append(delta)
 
-	#we can now compute weight changes for the input to hidden layer weights:
+	#to compute weight changes(using the delta rule, computing capital delta here):
 
+	#computing capital theta, i.e., weight change factor for hidden to output ayer weights, i.e., theta2:
+	capital_delta_theta2 = []					
 	for i in range(number_of_hidden_neurons):
-		for j in range(number_of_input_neurons):
-			
+		temp = []
+		for j in range(number_of_output_neurons):
+			temp.append(learning_rate * delta_output_layer[j] * network_output[j] * (1 - network_output[j]) * 
+						hidden_layer_output[i][j])
+		capital_delta_theta2.append(temp)
+
+	#computing cumulative output of each neuron in the hidden layer, requried for theta1:
+	hidden_neuron_cumulative_output = []
+	for i in range(number_of_hidden_neurons):
+		sum = 0
+		for j in range(number_of_output_neurons):
+			sum += hidden_layer_output[i][j]
+		hidden_neuron_cumulative_output.append(sum)
+
+	#computing capital theta for input to hidden layer weights, i.e., theta1:
+	capital_delta_theta1 = []
+	for i in range(number_of_input_neurons):
+		temp = []
+		for j in range(number_of_hidden_neurons):
+			temp.append(learning_rate * delta_hidden_layer[j] * hidden_neuron_cumulative_output[j] * 
+						(1 - hidden_neuron_cumulative_output[j]) * hidden_layer_input[i][j])
+		capital_delta_theta1.append(temp)
+
+	#computing new weights:		new_weight = capital_delta - old_weight:
+
+	#for theta2:
+	new_hidden_to_output_layer_wts = []
+	for i in range(number_of_hidden_neurons):
+		temp = []
+		for j in range(number_of_output_neurons):
+			temp.append(hidden_to_output_layer_wts[i][j] - capital_delta_theta2[i][j])
+		new_hidden_to_output_layer_wts.append(temp)
+
+	#for theta1:
+	new_input_to_hidden_layer_wts = []
+	for i in range(number_of_input_neurons):
+		temp = []
+		for j in range(number_of_hidden_neurons):
+			temp.append(input_to_hidden_layer_wts[i][j] - capital_delta_theta1[i][j])
+		new_input_to_hidden_layer_wts.append(temp)
+
+	return new_hidden_to_output_layer_wts, new_input_to_hidden_layer_wts
+	
